710e582a9255129411ed08fc009f6ae9aea26e9c
Phoenix <kahlo.phoenix@gmail.com>
2017-07-22 01:11:15 -0500

    remove files that don't belong

--- core/src_/com/phoenixkahlo/hellcraft/core/HUD.scala
+++
@@ -1,7 +0,0 @@
-package com.phoenixkahlo.hellcraft.core
-
-trait HUD {
-
-  def components(texturePack: TexturePack): Seq[HUDComponent]
-
-}
--- core/src_/com/phoenixkahlo/hellcraft/math/Shape.scala
+++
@@ -1,9 +0,0 @@
-package com.phoenixkahlo.hellcraft.math
-
-trait Shape {
-
-  def contains(p: V2F): Boolean
-
-  def closestPerimiterPoint(p: V2F): V2F
-
-}
