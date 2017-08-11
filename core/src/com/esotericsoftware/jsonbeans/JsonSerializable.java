8b314f4a6b904f800ba1b01bf50b9ffd92dd3e79
Phoenix <kahlo.phoenix@gmail.com>
2017-08-11 17:39:06 -0500

    remove kryonet from src

--- core/src/com/esotericsoftware/jsonbeans/JsonSerializable.java
+++
@@ -1,8 +0,0 @@
-
-package com.esotericsoftware.jsonbeans;
-
-public interface JsonSerializable {
-	public void write (com.esotericsoftware.jsonbeans.Json json);
-
-	public void read (com.esotericsoftware.jsonbeans.Json json, JsonValue jsonData);
-}
--- core/src/com/esotericsoftware/jsonbeans/JsonSerializer.java
+++
@@ -1,8 +0,0 @@
-
-package com.esotericsoftware.jsonbeans;
-
-public interface JsonSerializer<T> {
-	public void write (com.esotericsoftware.jsonbeans.Json json, T object, Class knownType);
-
-	public T read (com.esotericsoftware.jsonbeans.Json json, JsonValue jsonData, Class type);
-}
