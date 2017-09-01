1c7a61cec7ad718041a47384e32c24f85bd4ccf5
Phoenix <kahlo.phoenix@gmail.com>
2017-08-31 19:05:04 -0500

    progress

--- core/src/com/phoenixkahlo/hellcraft/singleplayer/Renderer.scala
+++ core/src/com/phoenixkahlo/hellcraft/singleplayer/Renderer.scala
@@ -1,12 +1,13 @@
 package com.phoenixkahlo.hellcraft.singleplayer
 
 import com.badlogic.gdx.Gdx
-import com.badlogic.gdx.graphics.{GL20, PerspectiveCamera}
+import com.badlogic.gdx.graphics.{GL20, PerspectiveCamera, Pixmap}
 import com.badlogic.gdx.graphics.g3d.shaders.DefaultShader
 import com.badlogic.gdx.graphics.g3d._
 import com.badlogic.gdx.graphics.g3d.attributes.ColorAttribute
 import com.badlogic.gdx.graphics.g3d.environment.DirectionalLight
 import com.badlogic.gdx.graphics.g3d.utils.{DefaultShaderProvider, ShaderProvider}
+import com.badlogic.gdx.graphics.glutils.FrameBuffer
 import com.badlogic.gdx.utils.Disposable
 import com.phoenixkahlo.hellcraft.graphics.ResourcePack
 import com.phoenixkahlo.hellcraft.graphics.`new`._
@@ -20,11 +21,13 @@ class Renderer(resources: ResourcePack) extends Disposable {
   val environment = new Environment
   environment.set(new ColorAttribute(ColorAttribute.AmbientLight, 0.4f, 0.4f, 0.4f, 1))
   environment.add(new DirectionalLight().set(1, 1, 1, 0, -1, 0))
+
   val cam = new PerspectiveCamera(70, Gdx.graphics.getWidth, Gdx.graphics.getHeight)
   cam.near = 0.1f
   cam.far = 1000
   cam.position.set(V3F(-10, 10, -10) toGdx)
   cam.lookAt(0, 10, 0)
+
   val sceneShader = new SceneShader(resources.sheet)
   sceneShader.init()
   val lineShader = new LineShader
@@ -48,7 +51,8 @@ class Renderer(resources: ResourcePack) extends Disposable {
     override def dispose(): Unit = ()
   })
 
-  //val batch = new ModelBatch(new DefaultShaderProvider)
+  val lightBuffer = new FrameBuffer(GL20.GL_DEPTH_COMPONENT16, 1024, 1024, true)
+
 
   def render(providers: Seq[RenderableProvider]): Unit = {
     Gdx.gl.glClearColor(0.5089f, 0.6941f, 1f, 1f)
