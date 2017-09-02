7362415b34ace483366ca02723abb7c9a9127850
Phoenix <kahlo.phoenix@gmail.com>
2017-09-01 20:06:29 -0500

    add shadow bias

--- core/assets/shaders/scene_f.glsl
+++ core/assets/shaders/scene_f.glsl
@@ -8,11 +8,14 @@ uniform sampler2D u_texture;
 uniform sampler2D u_depthMap;
 
 void main() {
+    float bias = 0.005;
     float visibility = 1.0;
-    if (texture2D(u_depthMap, v_shadowCoord.xy).a < v_shadowCoord.z) {
+    if (v_shadowCoord.z < 0) {
+        visibility = 0.4;
+    } else if ((v_shadowCoord.x < 0) || (v_shadowCoord.y < 0) || (v_shadowCoord.x >= 1) || (v_shadowCoord.y >= 1)) {
+        visibility = 0.4;
+    } else if (texture2D(u_depthMap, v_shadowCoord.xy).a < v_shadowCoord.z - bias) {
         visibility = 0.4;
     }
     gl_FragColor = texture2D(u_texture, v_texCoord0) * visibility;
-
-    //gl_FragColor = texture2D(u_texture, v_texCoord0);
 }
