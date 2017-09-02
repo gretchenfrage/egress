d8f320db77e7fc1dc21125ff7824d659973a77ee
Phoenix <kahlo.phoenix@gmail.com>
2017-09-01 19:10:02 -0500

    clean up depth shader

--- core/assets/shaders/depth_f.glsl
+++ core/assets/shaders/depth_f.glsl
@@ -3,10 +3,6 @@
 uniform float u_lightFar;
 uniform vec3 u_lightPos;
 
-varying vec4 v_realPos;
-
 void main() {
-    //gl_FragColor = vec4(gl_FragCoord.z / u_lightFar);
-    //gl_FragColor = vec4(vec3(length(v_realPos.xyz - u_lightPos) / u_lightFar), 1);
     gl_FragColor = vec4(gl_FragCoord.z);
 }
