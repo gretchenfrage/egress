94a565bd7dc9c89d12930241913081f5dfb47daf
Phoenix <kahlo.phoenix@gmail.com>
2018-01-10 19:24:25 -0600

    first person physics

--- core/src/com/phoenixkahlo/hellcraft/bullet/BulletTest.scala
+++ core/src/com/phoenixkahlo/hellcraft/bullet/BulletTest.scala
@@ -2,8 +2,9 @@ package com.phoenixkahlo.hellcraft.bullet
 
 import java.util.{Random, Scanner}
 
+import com.badlogic.gdx.Input.Keys
 import com.badlogic.gdx.graphics.g2d.TextureRegion
-import com.badlogic.gdx.{Gdx, InputProcessor}
+import com.badlogic.gdx.{Gdx, InputAdapter, InputProcessor}
 import com.badlogic.gdx.graphics.g3d.utils.FirstPersonCameraController
 import com.badlogic.gdx.math.{Matrix4, Vector3}
 import com.badlogic.gdx.physics.bullet.Bullet
@@ -21,6 +22,7 @@ import com.phoenixkahlo.hellcraft.math.MatrixFactory.Translate
 import com.phoenixkahlo.hellcraft.math._
 import com.phoenixkahlo.hellcraft.util.collections.spatial.Octree.Octree
 import com.phoenixkahlo.hellcraft.util.collections.spatial.{Octree, SpatialTemporalQueue}
+import com.phoenixkahlo.hellcraft.util.debugging.Profiler
 import com.phoenixkahlo.hellcraft.util.fields.IDField
 import com.phoenixkahlo.hellcraft.util.threading.UniExecutor
 
@@ -32,7 +34,7 @@ class BulletTestWorld {
   implicit val exec = ExecCheap
 
   //val rad = V3I(3, 2, 3)
-  val rad = Origin
+  val rad = V3I(3, 2, 3)
   val cdomain = rad.neg toAsSet rad
   val tdomain = cdomain.bloat
 
@@ -106,7 +108,8 @@ class BulletTest extends GameState {
 
   private var pack: ResourcePack = _
   private var renderer: Renderer = _
-  private var controller: FirstPersonCameraController = _
+  private var controller: InputAdapter = _
+  private var controllerUpdate: () => Unit = () => ()
 
   var chunks: BulletTestWorld = _
 
@@ -133,8 +136,8 @@ class BulletTest extends GameState {
 
     renderer = new DefaultRenderer(pack)
 
-    controller = new FirstPersonCameraController(renderer.cam)
-    Gdx.input.setInputProcessor(controller)
+    //controller = new FirstPersonCameraController(renderer.cam)
+    //Gdx.input.setInputProcessor(controller)
 
     chunks = new BulletTestWorld
 
@@ -164,7 +167,10 @@ class BulletTest extends GameState {
     groundCollObject = new btCollisionObject
     groundCollObject.setCollisionShape(groundShape)
     groundCollObject.setFriction(3)
+    val p = Profiler("add ground to world")
     world.addCollisionObject(groundCollObject)
+    p.log()
+    p.printMicro()
 
     println("computed ground shape")
 
@@ -177,6 +183,82 @@ class BulletTest extends GameState {
     orbRigidBody = new btRigidBody(orbConstrInfo)
     world.addRigidBody(orbRigidBody)
 
+    controller = new InputAdapter {
+      override def mouseMoved(screenX: Int, screenY: Int): Boolean = {
+        val camera = renderer.cam
+        val tmp = new Vector3
+        val degreesPerPixel = 0.5f
+        val deltaX = -Gdx.input.getDeltaX * degreesPerPixel
+        val deltaY = -Gdx.input.getDeltaY * degreesPerPixel
+        camera.direction.rotate(camera.up, deltaX)
+        tmp.set(camera.direction).crs(camera.up).nor
+        camera.direction.rotate(tmp, deltaY)
+        true
+      }
+
+
+      override def touchDown(screenX: Int, screenY: Int, pointer: Int, button: Int): Boolean = {
+        Gdx.input.setCursorCatched(true)
+        true
+      }
+
+      override def keyDown(keycode: Int): Boolean =
+        if (keycode == Keys.ESCAPE) {
+          Gdx.input.setCursorCatched(false)
+          true
+        } else false
+
+      def update(): Unit = {
+        import com.badlogic.gdx.Input.Keys._
+        def deltaTime = Gdx.graphics.getDeltaTime
+        val tmp = new Vector3
+        val camera = renderer.cam
+        val STRAFE_LEFT: Int = Keys.A
+        val STRAFE_RIGHT = Keys.D
+        val FORWARD = Keys.W
+        val BACKWARD = Keys.S
+        val UP = Keys.Q
+        val DOWN = Keys.E
+        val velocity = 5
+        var dx = new Vector3
+
+        if (Gdx.input.isKeyPressed(FORWARD)) {
+          tmp.set(camera.direction).nor.scl(deltaTime * velocity)
+          dx.add(tmp)
+        }
+        if (Gdx.input.isKeyPressed(BACKWARD)) {
+          tmp.set(camera.direction).nor.scl(-deltaTime * velocity)
+          dx.add(tmp)
+        }
+        if (Gdx.input.isKeyPressed(STRAFE_LEFT)) {
+          tmp.set(camera.direction).crs(camera.up).nor.scl(-deltaTime * velocity)
+          dx.add(tmp)
+        }
+        if (Gdx.input.isKeyPressed(STRAFE_RIGHT)) {
+          tmp.set(camera.direction).crs(camera.up).nor.scl(deltaTime * velocity)
+          dx.add(tmp)
+        }
+        if (Gdx.input.isKeyPressed(UP)) {
+          tmp.set(camera.up).nor.scl(deltaTime * velocity)
+          dx.add(tmp)
+        }
+        if (Gdx.input.isKeyPressed(DOWN)) {
+          tmp.set(camera.up).nor.scl(-deltaTime * velocity)
+          dx.add(tmp)
+        }
+
+        dx = (V3F(dx) * 10).toGdx
+
+        println("applying inpulse of " + dx)
+
+        orbRigidBody.applyCentralImpulse(dx)
+        orbRigidBody.activate()
+
+        camera.update(true)
+      }
+      controllerUpdate = update
+    }
+    Gdx.input.setInputProcessor(controller)
   }
 
   override def render(): Unit = {
@@ -189,10 +271,10 @@ class BulletTest extends GameState {
     mat.getTranslation(tra)
     val pos = V3F(tra)
 
-    controller.update()
+    controllerUpdate()
 
     val globals = GlobalRenderData(
-      V3F(renderer.cam.position), V3F(renderer.cam.direction),
+      pos, V3F(renderer.cam.direction),
       Up * 80, 1,
       V4I.ones, 90
     )
